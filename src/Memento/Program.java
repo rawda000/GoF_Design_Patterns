5110c32f8e0ea4704b7cf6e2f8fa2df034392e7e
Rawda Yasser <rawdayasser2019@gmail.com>
2020-11-17 00:11:19 +0200

    changed memento pattern to add multiple store points

--- src/Memento/Program.java
+++ src/Memento/Program.java
@@ -1,5 +1,8 @@
 package Memento;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Program {
     public static void main(String[] args) {
         System.out.println("*** Memento Pattern Demo ***");
@@ -9,5 +12,19 @@ public class Program {
         memento = originator.saveMemento(originator.getStateID());
         originator.setStateID(3);
         originator.revertMemento(memento);
+        System.out.println("-----------------------------------");
+        List<Memento> mementoList = new ArrayList<>();
+        originator.setStateID(4);
+        memento = originator.saveMemento(originator.getStateID());
+        mementoList.add(memento);
+        originator.setStateID(5);
+        memento = originator.saveMemento(originator.getStateID());
+        mementoList.add(memento);
+        originator.setStateID(6);
+        memento = originator.saveMemento(originator.getStateID());
+        mementoList.add(memento);
+        for (int i = mementoList.size() - 1; i >= 0; i--) {
+            originator.revertMemento(mementoList.get(i));
+        }
     }
 }
